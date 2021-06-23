ebfa935d711330d35cd401d870806e8a3a48bbb3
Belkowski656 <hub.mezykprogramming@gmail.com>
2021-06-23 13:56:44 +0200

    add sticky menu

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector('.nav__burger');
 const menu = document.querySelector('.nav__menu');
 const blure = document.querySelector('.blure');
+const navBox = document.querySelector('.nav__box');
 
 burger.addEventListener('click', () => {
     menu.classList.toggle('nav__menu--active');
@@ -11,3 +12,11 @@ blure.addEventListener('click', () => {
     menu.classList.remove('nav__menu--active');
     blure.classList.remove('blure--active');
 })
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 45) {
+        navBox.classList.add('scrolled');
+    } else {
+        navBox.classList.remove('scrolled');
+    }
+});
