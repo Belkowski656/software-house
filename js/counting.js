52c47f283da2316c2d26f34a2b0fdfe8bd9ce893
Belkowski656 <hub.mezykprogramming@gmail.com>
2021-06-23 17:06:03 +0200

    add counting animation

---
+++ js/counting.js
@@ -0,0 +1,36 @@
+const spans = document.querySelectorAll('.stats__stat');
+
+let spansFlags = [true, true, true, true];
+
+
+const counting = (span, index) => {
+    if (!spansFlags[index]) return;
+
+    const endValue = span.dataset.number * 1;
+    let value = 0;
+    let increaseValue = 5;
+
+    if (index === 0 || index === 3) increaseValue = 10;
+    if (index === 1) increaseValue = 1;
+    if (index === 2) increaseValue = 50;
+
+    setInterval(() => {
+        if (value === endValue) {
+            spansFlags[index] = false;
+            return;
+        };
+        value += increaseValue;
+        span.textContent = value;
+    }, 50);
+}
+
+const isVisible = () => {
+    spans.forEach((span, index) => {
+        if (span.getBoundingClientRect().top - span.offsetHeight + 120 < window.innerHeight) {
+            counting(span, index);
+        }
+    })
+};
+
+document.addEventListener('scroll', isVisible);
+isVisible();
