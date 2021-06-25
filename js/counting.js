2ba047da3a7f37d74a8255f0547bfdf33892e6af
Belkowski656 <hub.mezykprogramming@gmail.com>
2021-06-25 17:44:31 +0200

    add loading animation

--- js/counting.js
+++ js/counting.js
@@ -1,36 +1,38 @@
-const spans = document.querySelectorAll('.stats__stat');
+window.addEventListener('load', () => {
+    const spans = document.querySelectorAll('.stats__stat');
 
-let spansFlags = [true, true, true, true];
+    let spansFlags = [true, true, true, true];
 
 
-const counting = (span, index) => {
-    if (!spansFlags[index]) return;
+    const counting = (span, index) => {
+        if (!spansFlags[index]) return;
 
-    const endValue = span.dataset.number * 1;
-    let value = 0;
-    let increaseValue = 5;
+        const endValue = span.dataset.number * 1;
+        let value = 0;
+        let increaseValue = 5;
 
-    if (index === 0 || index === 3) increaseValue = 25;
-    if (index === 1) increaseValue = 1;
-    if (index === 2) increaseValue = 50;
+        if (index === 0 || index === 3) increaseValue = 25;
+        if (index === 1) increaseValue = 1;
+        if (index === 2) increaseValue = 50;
 
-    setInterval(() => {
-        if (value === endValue) {
-            spansFlags[index] = false;
-            return;
-        };
-        value += increaseValue;
-        span.textContent = value;
-    }, 50);
-}
+        setInterval(() => {
+            if (value === endValue) {
+                spansFlags[index] = false;
+                return;
+            };
+            value += increaseValue;
+            span.textContent = value;
+        }, 50);
+    }
 
-const isVisible = () => {
-    spans.forEach((span, index) => {
-        if (span.getBoundingClientRect().top - span.offsetHeight + 120 < window.innerHeight) {
-            counting(span, index);
-        }
-    })
-};
+    const isVisible = () => {
+        spans.forEach((span, index) => {
+            if (span.getBoundingClientRect().top - span.offsetHeight + 120 < window.innerHeight) {
+                counting(span, index);
+            }
+        })
+    };
 
-document.addEventListener('scroll', isVisible);
-isVisible();
+    document.addEventListener('scroll', isVisible);
+    isVisible();
+});
---
+++ js/loading.js
@@ -0,0 +1,5 @@
+const loading = document.querySelector('.loading-bg');
+
+window.addEventListener('load', () => {
+    loading.style.display = "none";
+});
